8c62a8c0113d12d56d7609e767d6dc133ef2bdc8
Serhii Ruban <sergii.ruban@gmail.com>
2026-04-30 15:05:03 +0000

    Add data output to stdout. Add additional error checking. Initilizeoutput variables

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <iomanip>
 
 constexpr int ticks_per_revolution   = 1024;
-constexpr float wheel_radius_m       = 0.3;
-constexpr float wheelbase_m          = 1.0;
+constexpr double wheel_radius_m      = 0.3;
+constexpr double wheelbase_m         = 1.0;
 constexpr double distance_per_tick   = 2.0 * M_PI * wheel_radius_m / ticks_per_revolution;
 
 void calcPosition(double d_left, double d_right, double& outX, double& outY, double& outTheta) {
@@ -17,7 +18,6 @@ void calcPosition(double d_left, double d_right, double& outX, double& outY, dou
     outY     += d * sin(outTheta + dtheta / 2);
     outTheta += dtheta;
 }
-
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -43,34 +43,34 @@ int main(int argc, char** argv) {
     long bl_ticks;
     long br_ticks;
 
-    double x;
-    double y;
-    double theta;
+    double x{};
+    double y{};
+    double theta{};
 
     inputfs >> timestamp_ms >> prev_fl_ticks >> prev_fr_ticks >> prev_bl_ticks >> prev_br_ticks;
+    if (!inputfs) {
+        std::cerr << "Invalid or empty input file " << argv[1] << " \n";
+        return 1;
+    }
 
-    while(inputfs) {
-        inputfs >> timestamp_ms >> fl_ticks >> fr_ticks >> bl_ticks >> br_ticks;
-
+    while (inputfs >> timestamp_ms >> fl_ticks >> fr_ticks >> bl_ticks >> br_ticks) {
         long d_fl = fl_ticks - prev_fl_ticks;
         long d_fr = fr_ticks - prev_fr_ticks;
         long d_bl = bl_ticks - prev_bl_ticks;
         long d_br = br_ticks - prev_br_ticks;
 
-        long d_left  = (d_fl + d_bl) / 2;
-        long d_right = (d_fr + d_br) / 2;
+        double d_left  = (d_fl + d_bl) / 2.0;
+        double d_right = (d_fr + d_br) / 2.0;
 
         calcPosition(d_left, d_right, x, y, theta);
+
+        std::cout << timestamp_ms << std::fixed << std::setprecision(5) << " " << x << " " << y << " " << theta << "\n";
 
         prev_fl_ticks = fl_ticks;
         prev_fr_ticks = fr_ticks;
         prev_bl_ticks = bl_ticks;
         prev_br_ticks = br_ticks;
     }
-
-    // TODO: implement wheel odometry for a 4-wheel differential-drive UGV.
-    // Output: same tabular format on stdout, starting from the second sample:
-    //         timestamp_ms x y theta
 
     return 0;
 }
