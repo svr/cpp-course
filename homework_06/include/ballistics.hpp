b1b322a5436597d30e92f6b65e1dc7b551bc98f4
Serhii Ruban <sergii.ruban@gmail.com>
2026-06-06 10:39:15 +0000

    homework_06: format file with clangd-format

--- homework_06/include/ballistics.hpp
+++ homework_06/include/ballistics.hpp
@@ -3,29 +3,25 @@
 constexpr int MAX_AMMO_NAME_LENGTH = 16;
 constexpr float EPS = 1e-6F;
 
-enum class StatusCode {
-    Ok = 0,
-    UknownAmmo = 1,
-    CalcEror = 3
-};
+enum class StatusCode { Ok = 0, UknownAmmo = 1, CalcEror = 3 };
 
 struct BallisticsInput {
-    float drone_x;
-    float drone_y;
-    float drone_z;
-    float target_x ;
-    float target_y;
-    float attack_speed;
-    float acceleration_path;
-    char ammo_name[MAX_AMMO_NAME_LENGTH];
+  float drone_x;
+  float drone_y;
+  float drone_z;
+  float target_x;
+  float target_y;
+  float attack_speed;
+  float acceleration_path;
+  char ammo_name[MAX_AMMO_NAME_LENGTH];
 };
 
 struct DropSolution {
-    float fire_x;
-    float fire_y;
-    float drone_x;
-    float drone_y;
-    StatusCode status_code;
+  float fire_x;
+  float fire_y;
+  float drone_x;
+  float drone_y;
+  StatusCode status_code;
 };
 
 DropSolution compute_drop_solution(const BallisticsInput& input);
